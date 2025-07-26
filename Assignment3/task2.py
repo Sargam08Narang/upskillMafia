c4c2b0759c4313a8615110a9707f77545f9b213f
Sargam08Narang <127529732+Sargam08Narang@users.noreply.github.com>
2025-07-26 17:42:54 +0530

    initial commit

---
+++ Assignment3/task2.py
@@ -0,0 +1,9 @@
+x=int(input("Enter a number:"))
+import math
+square_root=math.sqrt(x)
+log_num=math.log(x)
+sine_function=math.sin(x)
+
+print("Square root:",square_root)
+print("Logarithm:",log_num)
+print("Sine:",sine_function)
